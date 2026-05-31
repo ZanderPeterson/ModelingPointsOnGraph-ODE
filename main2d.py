7e1cc892a3ffcc5343669e76794b12cb74b2cbc3
Zander Peterson <zander.j.peterson@gmail.com>
2026-05-31 18:07:32 +1200

    Simulate Points

--- main2d.py
+++ main2d.py
@@ -1,6 +1,7 @@
 # Standard Libraries
 from collections.abc import Callable
 import math
+import time
 
 # Third-Party Libraries
 import numpy as np
@@ -9,16 +10,30 @@ import matplotlib.pyplot as plt
 
 # Functions (Can be changed, but must be ufunc (numpy arrays can be passed through it))
 main_function: np.ufunc = lambda x: (np.sin(x))
+main_derivative: np.ufunc = lambda x: (np.cos(x))
+
+FRICTION: float = 0.8
+GRAVITY: float = 9.81
+differential_eq: np.ufunc = lambda x, v: (-v*FRICTION - GRAVITY*np.sin(np.atan(main_derivative(x))))
+
+# Points (Written as an array of [x, v] pairs, each being a point [[x, v, a], ..., [x, v, a]])
+# Note: a will be overwritten and thus will not change anything.
+simulated_points: npt.NDArray[np.float64] = np.array([[0.0, 0.0, 0.0],
+                                                      [-1.0, 10.0, 0.0]])
+SIMULATION_TIME_STEP: float = 0.0001  # Smaller steps in time allows for more accuracy, at the cost of processing power
+simulation_time_s: float = 0  # The time, in seconds, since the simulation began
+start_time_s: float = time.perf_counter()
 
 # Constants pertaining to drawing the continuous function
-FUNCTION_DETAIL: float = 0.01
-HORIZONTAL_GRAPH_MIN: float = -5
-HORIZONTAL_GRAPH_MAX: float = 5
+FUNCTION_DETAIL: float = 0.001
+HORIZONTAL_GRAPH_MIN: float = -10
+HORIZONTAL_GRAPH_MAX: float = 10
 
 if __name__ == "__main__":
     print("Running main2d.py")
 
     plt.style.use('dark_background')  # Sets the theme
+    plt.ion()  # Allows for interactions w/loops
 
     x_range: float = HORIZONTAL_GRAPH_MAX - HORIZONTAL_GRAPH_MIN
     x_steps: int = int(x_range/FUNCTION_DETAIL)
@@ -32,4 +47,18 @@ if __name__ == "__main__":
     ax.set_ylabel("Y Values")
     ax.set_title("Main Function")
     ax.grid(True, alpha=0.4)
-    plt.show()
+
+    # Main loop
+    points, = plt.plot(0, 0, 'r')
+    while plt.fignum_exists(fig.number):
+        current_time_s: float = time.perf_counter()
+        while (current_time_s - simulation_time_s) > start_time_s:
+            simulation_time_s += SIMULATION_TIME_STEP
+            simulated_points[:, 2] = differential_eq(simulated_points[:, 0], simulated_points[:, 1])
+            simulated_points[:, 0] += simulated_points[:, 1]*SIMULATION_TIME_STEP
+            simulated_points[:, 1] += simulated_points[:, 2]*SIMULATION_TIME_STEP
+
+        points.remove()
+        points, = plt.plot(simulated_points[:, 0], main_function(simulated_points[:, 0]), 'ro')
+        plt.pause(0.05)
+    print("Figure closed")
